baa61c51cebde92bfc5334e2ff688ac577d4c4b2
gulfamsaifi931 <147502040+gulfamsaifi931@users.noreply.github.com>
2023-10-17 12:36:27 +0530

    commit dd commit

--- src/app/contacts/page.js
+++ src/app/contacts/page.js
@@ -50,7 +50,7 @@ const Contact = () => {
         <div className="flex flex-wrap">
         
       <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-black">Contact Us</h2>
-      <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">If you have any query then you can contect with our team. thank you from A Plus studio.</p>
+      <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">If you have any query then you can contect with our team. thank you from A Plus studio.If you have any query then you can contect with our team.</p>
      
      
      
