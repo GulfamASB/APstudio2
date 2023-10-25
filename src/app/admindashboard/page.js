6b3ec305047ec493549c9d6926cb73691fb655b1
gulfamsaifi931 <147502040+gulfamsaifi931@users.noreply.github.com>
2023-10-25 18:09:15 +0530

    fldnfjddnsdn commit

--- src/app/admindashboard/page.js
+++ src/app/admindashboard/page.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-
+import Post from '@/app/adminpost/[post]/page'
+import ProductList from '@/app/adminpost/[post]/[productList]/page'
 
 const Admin = () => {
   return (
@@ -135,9 +136,9 @@ const Admin = () => {
               <p class="mb-6 text-gray-600">Here's is your Products posts....</p>
               <div class="flex flex-wrap gap-x-4 gap-y-8">
               <div class="h-96 w-full rounded-xl bg-white p-10 shadow-md">
-
+                <Post />
               </div>
-
+              <ProductList/>
                 <div class="h-56 w-full rounded-xl bg-white p-10 shadow-md">
                 </div>
                 <div class="h-56 w-full rounded-xl bg-white p-10 shadow-md"></div>
@@ -151,5 +152,6 @@ const Admin = () => {
     </>
   );
 };
+
 
 export default Admin
