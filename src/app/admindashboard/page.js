6d562e4230936d45e8ed4cabed49e359ba037422
gulfamsaifi931 <147502040+gulfamsaifi931@users.noreply.github.com>
2023-10-26 10:19:26 +0530

    llll commit

--- src/app/admindashboard/page.js
+++ src/app/admindashboard/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import Post from '@/app/createproduct/page'
-import ProductList from '@/app/productList/page'
+
 
 const Admin = () => {
   return (
