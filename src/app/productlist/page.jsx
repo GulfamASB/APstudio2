6a681ff78acbff879384df4612e861f2b6edbabb
gulfamsaifi931 <147502040+gulfamsaifi931@users.noreply.github.com>
2023-10-26 13:13:03 +0530

    hhuuu

--- src/app/productlist/page.jsx
+++ src/app/productlist/page.jsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import RemoveBtn from "../components/RemoveBtn";
+import { HiPencilAlt } from "react-icons/hi";
 
 const getProducts = async () => {
   try {
@@ -21,20 +23,20 @@ const getProducts = async () => {
 
   return (
     <>
-      {products.map((t) => (
+      {products && products.map((t) => (
         <div
           key={t._id}
           className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
         >
           <div>
-            <img className="font-bold text-2xl h-40 " src={t.mediaUrl}  />
+            <img className="font-bold text-2xl h-40 " src={t.mediaUrl} alt='' />
             <div>{t.name}</div>
           </div>
 
           <div className="flex gap-2">
-
+            <RemoveBtn id={t._id} />
             <Link href={`/`}>
-
+              <HiPencilAlt size={24} />
             </Link>
           </div>
         </div>
