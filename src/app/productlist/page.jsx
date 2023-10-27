f919deeed743722b62e745b32844eeb32e4373d7
gulfamsaifi931 <147502040+gulfamsaifi931@users.noreply.github.com>
2023-10-27 17:14:25 +0530

    sdf

--- src/app/productlist/page.jsx
+++ src/app/productlist/page.jsx
@@ -1,33 +1,44 @@
+import Link from "next/link";
+import RemoveBtn from "../components/RemoveBtn";
+import { HiPencilAlt } from "react-icons/hi";
+const getTopics = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/products", {
+      cache: "no-store",
+    });
 
+    if (!res.ok) {
+      throw new Error("Failed to fetch products");
+    }
 
- const AdminPage = async () =>{
-  const products = await getServerSideProps();
-  return(
-    <div>
-    {products.map((products) => (
+    return res.json();
+  } catch (error) {
+    console.log("Error loading products: ", error);
+  }
+};
+
+export default async function ProductsList() {
+  const { products } = await getTopics();
+  return (
+    <>
+      {products.map((t) => (
         <div
-          key={products._id}
+          key={t._id}
           className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
         >
           <div>
-            <img className="font-bold text-2xl h-40 " src={products.mediaUrl} alt='' />
-            <div>{products.name}</div>
+            <img className="font-bold text-2xl h-40 " src={t.mediaUrl} alt='' />
+            <div>{t.name}</div>
           </div>
 
-         
+          <div className="flex gap-2">
+            <RemoveBtn id={t._id} />
+            <Link href={`/`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
         </div>
       ))}
-       
-      </div>
+    </>
   );
 };
-export async function getServerSideProps() {
-  const product = await fetch("http://localhost:3000/api/showproduct", {
-    cache: "force-cache",
-  });
-  const data = await product.json();
-  return data.products;
-};
-
-export default AdminPage;
-
