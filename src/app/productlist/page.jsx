0ea6f4a561082b47d471f05c453319ab74416e49
gulfamsaifi931 <147502040+gulfamsaifi931@users.noreply.github.com>
2023-10-27 12:53:50 +0530

    sdfgdf

--- src/app/productlist/page.jsx
+++ src/app/productlist/page.jsx
@@ -1,31 +1,36 @@
 import Link from "next/link";
 import RemoveBtn from "../components/RemoveBtn";
 import { HiPencilAlt } from "react-icons/hi";
+const getTopics = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/products", {
+      cache: "no-store",
+    });
 
-// This gets called on every request
-export async function getServerSideProps() {
-  // Fetch products from external API
-  const res = await fetch(`http://localhost:3000/api/showproduct`)
-  const products = await res.json()
- 
-  // Pass products to the page via props
-  return { props: { products } }
-}
-const Products = ({ products }) =>{  
+    if (!res.ok) {
+      throw new Error("Failed to fetch products");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log("Error loading products: ", error);
+  }
+};
+
+export default async function ProductsList() {
+  const { products } = await getTopics();
   return (
     <>
-    <div className="py-24 font-extrabold ">
-      <h1>Products List page</h1>
-    </div>
-      {products && products.map((t) => (
+      {products.map((t) => (
         <div
           key={t._id}
           className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
         >
           <div>
-            <img className=" font-bold text-2xl h-40 " src={t.mediaUrl} alt='' />
+            <img className="font-bold text-2xl h-40 " src={t.mediaUrl} alt='' />
             <div>{t.name}</div>
           </div>
+
           <div className="flex gap-2">
             <RemoveBtn id={t._id} />
             <Link href={`/`}>
@@ -37,4 +42,3 @@ const Products = ({ products }) =>{
     </>
   );
 };
-export default Products
