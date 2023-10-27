638a22781ef2049ef6d3f3a40b4015adfc876289
gulfamsaifi931 <147502040+gulfamsaifi931@users.noreply.github.com>
2023-10-27 16:32:55 +0530

    dfdsf

--- src/app/productlist/page.jsx
+++ src/app/productlist/page.jsx
@@ -1,36 +1,31 @@
-import Link from "next/link";
-
-const getTopics = async () => {
-  try {
-    const res = await fetch("http://localhost:3000/api/products", {
-      cache: "no-store",
+export const getProduct = async () => {
+    const product = await fetch("http://localhost:3000/api/products", {
+      cache: "force-cache",
     });
+    const data = await product.json();
+    return data.products;
+  };
 
-    if (!res.ok) {
-      throw new Error("Failed to fetch products");
-    }
 
-    return res.json();
-  } catch (error) {
-    console.log("Error loading products: ", error);
-  }
-};
-
-export default async function ProductsList() {
-  const { products } = await getTopics();
-  return (
+const AdminPage = async () =>{
+  const products = await getProduct();
+  return(
     <div>
-      {products.map((t) => (
-        <div
-          key={t._id}
-          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
-        >
-          <div>
-            <img className="font-bold text-2xl h-40 " src={t.mediaUrl} alt='' />
-            <div>{t.name}</div>
-          </div>
+      <h1>Product List Page</h1>
+      {products.map((products)=>(
+        <div key={products.id}>
+          <h1>{products.name}</h1>
         </div>
       ))}
-    </div>
+      </div>
   );
 };
+
+//export const getServerSideProps = async () =>{
+//  const data = await fetch("http://localhost:300/api/products")
+ // const product = await data.json();
+  //const  products = product.products;
+  //return {props:{products}};
+//};
+
+export default AdminPage;
