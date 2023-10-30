2a010cd42f5b5989cb78b0f29ca78f5455f9d226
gulfamsaifi931 <147502040+gulfamsaifi931@users.noreply.github.com>
2023-10-30 15:39:16 +0530

    fgfsdg

--- src/app/productlist/page.jsx
+++ src/app/productlist/page.jsx
@@ -3,26 +3,26 @@ import RemoveBtn from "../components/RemoveBtn";
 import { HiPencilAlt } from "react-icons/hi";
 const getTopics = async () => {
   try {
-    const res = await fetch("http://localhost:3000/api/showproduct", {
+    const res = await fetch("http://localhost:3000/api/products", {
       cache: "no-store",
     });
 
     if (!res.ok) {
-      throw new Error("Failed to fetch productlist");
+      throw new Error("Failed to fetch products");
     }
 
     return res.json();
   } catch (error) {
-    console.log("Error loading productlist: ", error);
+    console.log("Error loading products: ", error);
   }
 };
 
 export default async function ProductsList() {
   
-  const { productlist } = await getTopics();
+  const { products } = await getTopics();
   return (
     <>
-      {productlist.map((t) => (
+      {products.map((t) => (
         <div
           key={t._id}
           className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
@@ -43,3 +43,5 @@ export default async function ProductsList() {
     </>
   );
 };
+
+
