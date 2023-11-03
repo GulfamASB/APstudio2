c678da6db143e9c2e357f724b1e16bb7c26ad777
gulfamsaifi931 <147502040+gulfamsaifi931@users.noreply.github.com>
2023-11-03 12:45:14 +0530

    it is the latest one commit

--- src/app/productlist/page.jsx
+++ src/app/productlist/page.jsx
@@ -22,7 +22,7 @@ export default async function ProductsList() {
   const { products } =  await getTopics ()|| {}
   return (
     <>
-      {products?.map((t) => (
+      {products.map((t) => (
         <div
           key={t._id}
           className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
