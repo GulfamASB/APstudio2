2ced1d14e2178e87447da57dde4316bb5d774b9c
gulfamsaifi931 <147502040+gulfamsaifi931@users.noreply.github.com>
2023-11-03 11:19:46 +0530

    wefswfgsd

--- src/app/productlist/page.jsx
+++ src/app/productlist/page.jsx
@@ -18,7 +18,8 @@ const getTopics = async () => {
 };
 
 export default async function ProductsList() {
-  const { products } = await  getTopics(undefined);
+  const { products } = await getTopics();
+  console.log(products);
   return (
     <>
       {products.map((t) => (
