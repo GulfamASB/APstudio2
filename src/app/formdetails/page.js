9ff785363dad625f304cb29b7b2e37f955dd9257
gulfamsaifi931 <147502040+gulfamsaifi931@users.noreply.github.com>
2023-11-11 17:21:13 +0530

    it is the commit

--- src/app/formdetails/page.js
+++ src/app/formdetails/page.js
@@ -4,14 +4,14 @@ import Formdetails from "@/app/components/Formdetails";
 const Contact = () => {
     return (
         <>
-        <div class="bg-cover bg-center bg-[url('/AP10.jpg')] bg-fixed">
-<div  className={styles.container}>
-                <section className={styles.contact_section}>
-                <h1>order details</h1>
-                    <h2 >We'd love to hear <span> from you </span></h2>
-                    <Formdetails />
-                 </section>
-            </div>
+            <div style={{marginBottom: '-6rem'}} class="bg-cover bg-center bg-[url('/AP10.jpg')] bg-fixed">
+                <div className={styles.container}>
+                    <section className={styles.contact_section}>
+                        <h1>order details</h1>
+                        <h2 >We'd love to hear <span> from you </span></h2>
+                        <Formdetails />
+                    </section>
+                </div>
             </div>
         </>
     );
