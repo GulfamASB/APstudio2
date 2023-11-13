b63ff40988ddb70aaee25e3a666ba6cfc3767099
gulfamsaifi931 <147502040+gulfamsaifi931@users.noreply.github.com>
2023-11-13 16:31:27 +0530

    it is the commit

--- src/app/contacts/page.js
+++ src/app/contacts/page.js
@@ -82,7 +82,7 @@ const Contact = () => {
                       Email
                     </p>
                     <p className="text-neutral-500 dark:text-neutral-00">
-                      Info@aplusstudio.in
+                      info@aplusstudio.in
                     </p>
                     <p className="text-neutral-500 dark:text-neutral-200">Monday to Saturday Expected </p>
                         <p className="text-neutral-500 dark:text-neutral-200">response time: 24 hours </p>
