b3977ed313858ec2144c4e957f6c910fd99fc702
gulfamsaifi931 <147502040+gulfamsaifi931@users.noreply.github.com>
2023-11-18 12:14:17 +0530

    fgfd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body style={{fontFamily:'bodoni mt'}} >
+      <body >
         <Nav />
         {children}
       <Footer/>
