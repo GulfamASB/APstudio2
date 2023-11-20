e7ccd563ccca72994de3c8dd6eed179c75fda297
gulfamsaifi931 <147502040+gulfamsaifi931@users.noreply.github.com>
2023-11-20 14:26:41 +0530

    gergDGB

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import Nav from "./components/Nav"
 import localFont from 'next/font/local'
 
 
-const myFont = localFont({ src: "../fonts/bodoni-mt-1.ttf" })
+const myFont = localFont({ src: "./fonts/bodoni-mt-1.ttf" })
 
 export const metadata: Metadata = {
   title: 'A Plus Studio',
