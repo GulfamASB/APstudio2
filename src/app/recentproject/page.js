a7112ee00aa86dd23ed70da5307e30f1ad360cdc
gulfamsaifi931 <147502040+gulfamsaifi931@users.noreply.github.com>
2023-11-20 15:18:22 +0530

    it is the commit ooo commit

--- src/app/recentproject/page.js
+++ src/app/recentproject/page.js
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const Recentproject = () => {
   return (
     <>
-   <div  className=" py-24 bg-white">
+   <div  className=" py-6 bg-white">
         <div className="  md:py-16 w-11/12 lg:w-10/12 xl:w-1200 m-auto">
        
           <div className="space-y-16">
@@ -24,38 +24,39 @@ const Recentproject = () => {
             
               <div className="mx-auto w-1/1 md:w-1/3">
                
-<div class="grid grid-cols-2 md:grid-cols-2 gap-4">
-    <div class="relative  overflow-hidden  rounded-lg   ">
+<div className="grid grid-cols-2 md:grid-cols-2 gap-4">
+    <div className="relative  overflow-hidden  rounded-lg   ">
     <Image 
         width={10000}
         height={10000}
-        class="h-64 w-80 rounded-lg " src="/AP6.jpg" alt=""/>
+        className="h-64 w-80 rounded-lg " src="/AP6.jpg" alt=""/>
       <div
-        class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-30 "></div>
+        className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-30 "></div>
+    <p style={{marginTop:'-7rem'}} className="bg-opacity-40 relative  bg-black  text-white md:justify-center md:flex md:items-center">A Plus Studio</p>
     </div>
-    <div class="relative max-w-xs overflow-hidden bg-cover rounded-lg  bg-no-repeat">
+    <div className="relative max-w-xs overflow-hidden bg-cover rounded-lg  bg-no-repeat">
     <Image 
         width={10000}
         height={10000}
-        class="h-64 w-80 rounded-lg " src="/02.jpg" alt=""/>
+        className="h-64 w-80 rounded-lg " src="/02.jpg" alt=""/>
       <div
-        class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-30 "></div>
+        className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-30 "></div>
     </div>
-    <div class="relative max-w-xs overflow-hidden bg-cover rounded-lg  bg-no-repeat">
+    <div className="relative max-w-xs overflow-hidden bg-cover rounded-lg  bg-no-repeat">
     <Image 
         width={10000}
         height={10000}
-        class="h-64 w-80 rounded-lg " src="/AP7.jpg" alt=""/>
+        className="h-64 w-80 rounded-lg " src="/AP7.jpg" alt=""/>
       <div
-        class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-30 "></div>
+        className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-30 "></div>
     </div>
-    <div class="relative max-w-xs overflow-hidden bg-cover rounded-lg  bg-no-repeat">
+    <div className="relative max-w-xs overflow-hidden bg-cover rounded-lg  bg-no-repeat">
     <Image 
         width={10000}
         height={10000}
-        class="h-64 w-80 rounded-lg " src="/a21.jpg" alt=""/>
+        className="h-64 w-80 rounded-lg " src="/a21.jpg" alt=""/>
       <div
-        class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-30 "></div>
+        className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-30 "></div>
     </div>
 
     
